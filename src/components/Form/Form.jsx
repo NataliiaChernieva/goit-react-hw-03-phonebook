72a5fe6de2e6188780a3f7de501db0e74c0ea7a7
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-07-18 19:27:27 +0300

    редактирует инпут

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -36,6 +36,7 @@ export default class Form extends Component {
             <CustomForm onSubmit={this.handleAddContact}>
                 <Input type="text"
                     name="name"
+                    value={this.state.name}
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
                     required
@@ -43,6 +44,7 @@ export default class Form extends Component {
                 />
                 <Input type="tel"
                     name="number"
+                    value={this.state.number}
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
                     required
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import { CurrentInput, CurrentLabel } from './Input.styled';
 
-export default function Input({type, name, onChange}) {
+export default function Input({type, name, value, pattern=null, title=null, onChange}) {
   return(
     <CurrentLabel>{name}
       <CurrentInput
         type={type}
         name={name}
+        value={value}
+        pattern={pattern}
+        title={title}
         onChange={onChange}
       />
     </CurrentLabel>
